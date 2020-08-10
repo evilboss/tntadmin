c3d0ddc5aead0acac418d2c879c18d7beb07eb1c
evilboss <jr.evilboss@gmail.com>
2020-08-11 01:31:47 +0800

    add dynamic events on change order status

--- application/views/admin/orders/show.php
+++ application/views/admin/orders/show.php
@@ -24,23 +24,36 @@
                     <div class="box-body">
                         <?php print_r($record); ?>
                         <table class="table table-bordered">
+                            <thead>
+                            <th>Field</th>
+                            <th>Field</th>
+                            <th>Actions</th>
+
+                            </thead>
                             <tbody id="orderTableBody">
                             <tr>
                                 <th>Id</th>
                                 <td><?php echo xss_clean($record->id) ?></td>
+                                <td></td>
                             </tr>
                             <tr>
                                 <th>User</th>
                                 <td><?php echo xss_clean($record->user_name) ?></td>
+                                <td></td>
+
                             </tr>
 
                             <tr>
                                 <th>Email</th>
                                 <td><?php echo xss_clean($record->email) ?></td>
+                                <td></td>
+
                             </tr>
                             <tr>
                                 <th>Delivery address</th>
                                 <td><?php echo xss_clean($record->delivery_address) ?></td>
+                                <td></td>
+
                             </tr>
                             <tr>
                                 <th>Total order weight:</th>
@@ -55,24 +68,34 @@
                             <tr>
                                 <th>Status</th>
                                 <td><?php echo xss_clean($record->order_status) ?>
+                                </td>
+                                <td>
                                     <button id="changeStatus" class="btn btn-success" type="button">Change</button>
                                     <select id="status-select" class="hide">
-                                        <option value="paid">
-                                            Paid
+                                        <option value="ordered">
+                                            Ordered
                                         </option>
+                                        <option value="processing">
+                                            Processing
+                                        </option>
+
                                         <option value="for payment">
                                             For Payment
                                         </option>
-                                        <option value="For Shipping">
+                                        <option value="paid">
+                                            Paid
+                                        </option>
+                                        <option value="for shipping">
                                             For Shipping
                                         </option>
-                                        <option value="Shipped">
+                                        <option value="shipped">
                                             Shipped
                                         </option>
-                                        <option value="Delivered">
+                                        <option value="delivered">
                                             Delivered
                                         </option>
                                     </select>
+
                                 </td>
                             </tr>
                             </tbody>
@@ -89,19 +112,69 @@
 <script>
     $(function () {
 
-        $('#changeStatus').click(function () {
+        $('#changeStatus').on('click', function () {
             console.log('changes status click');
             $('#changeStatus').addClass('hide');
             $('#status-select').removeClass('hide');
         });
+
         $('#status-select').on('change', function (e) {
             console.log(e.currentTarget.value);
-            $('#orderTableBody').append(_.template(`<tr>
+            $('tr').remove('.option-row');
+
+            switch (e.currentTarget.value) {
+                case 'for payment':
+                    $('#orderTableBody').append(_.template(`<tr class="option-row">
                                 <th>Set Shipping Rate:</th>
                                 <td><input class="form-control"/></td>
-                                <td><button class="btn btn-success">Save</button></td>
+                                <td><button id="saveBtn" class="btn btn-success save">Save</button></td>
 
                             </tr>`));
+                    $('.save').on('click', function () {
+                        console.log('save button');
+                    });
+                    break;
+                case 'processing':
+                    $('#orderTableBody').append(_.template(`<tr class="option-row">
+                                <th>Set Transaction number:</th>
+                                <td><input class="form-control"/></td>
+                                <td><button class="btn btn-success save">Save</button></td>
+
+                            </tr>`));
+                    $('.save').on('click', function () {
+                        console.log('save button');
+                    });
+                    // code block
+                    break;
+                case 'paid':
+                    $('#orderTableBody').append(_.template(`<tr class="option-row">
+                                <th>Set Transaction number:</th>
+                                <td><input class="form-control"/></td>
+                                <td><button class="btn btn-success save">Save</button></td>
+
+                            </tr>`));
+                    $('.save').on('click', function () {
+                        console.log('save button');
+                    });
+                    // code block
+                    break;
+                case 'shipped':
+                    $('#orderTableBody').append(_.template(`<tr class="option-row">
+                                <th>Set shipping number:</th>
+                                <td><input class="form-control"/></td>
+                                <td><button class="btn btn-success save">Save</button></td>
+
+                            </tr>`));
+                    $('.save').on('click', function () {
+                        console.log('save button');
+                    });
+                    break;
+                default:
+
+                // code block
+            }
+
         });
+
     });
 </script>
