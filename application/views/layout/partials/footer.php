2f2c74beaae0582616043b35cd2b9dfb29733d32
evilboss <jr.evilboss@gmail.com>
2020-08-15 12:21:10 +0800

    add mx-0

--- application/views/layout/partials/footer.php
+++ application/views/layout/partials/footer.php
@@ -1,26 +1,24 @@
 <section id="wrapper-footer" class="gray-section text-white">
-    <div>
-        <div class="row">
-            <div class="col-md-3 col-sm-12"><h3>Facebook</h3>
-                <div class="fb-page"
-                     data-tabs="events"
-                     data-href="https://www.facebook.com/toyntoys/"
-                     data-width="380"></div>
-            </div>
-            <div class="col-md-3 col-sm-12"><h3>Instagram</h3>
-                To be added
-            </div>
+    <div class="row mx-0">
+        <div class="col-md-3 col-sm-12"><h3>Facebook</h3>
+            <div class="fb-page"
+                 data-tabs="events"
+                 data-href="https://www.facebook.com/toyntoys/"
+                 data-width="380"></div>
+        </div>
+        <div class="col-md-3 col-sm-12"><h3>Instagram</h3>
+            To be added
+        </div>
 
-            <div class="col-md-3 col-sm-12"><h3>Twitter</h3>
-                To be added
-            </div>
-            <div class="col-md-3 col-sm-12"><h3>Subscribe to our news letter</h3>
-                To follow
-            </div>
-
+        <div class="col-md-3 col-sm-12"><h3>Twitter</h3>
+            To be added
+        </div>
+        <div class="col-md-3 col-sm-12"><h3>Subscribe to our news letter</h3>
+            To follow
         </div>
 
     </div>
+
 </section>
 
 
