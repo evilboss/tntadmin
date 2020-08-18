b646290ee28cad2ed18b0d11baa9c98a0a6618bc
evilboss <jr.evilboss@gmail.com>
2020-08-18 21:15:04 +0800

    add copyright text

--- application/views/layout/partials/footer.php
+++ application/views/layout/partials/footer.php
@@ -1,24 +1,35 @@
-<section id="wrapper-footer" class="gray-section text-white">
-    <div class="row mx-0 text-center">
-        <div class="col-md-3 col-sm-12">
-            <h3 class="mb-4">Facebook</h3>
-            <div class="fb-page"
-                 data-tabs="events"
-                 data-href="https://www.facebook.com/toyntoys/"
-                 data-width="380"></div>
-        </div>
-        <div class="col-md-3 col-sm-12"><h3>Instagram</h3>
-            To be added
-        </div>
+<section id="wrapper-footer" class="">
+    <div class="dark-section">
+        <div class="container text-white">
+            <div class="row  text-center">
+                <div class="col-md-3 col-sm-12">
+                    <h3 class="mb-4">Facebook</h3>
+                    <div class="fb-page"
+                         data-tabs="events"
+                         data-href="https://www.facebook.com/toyntoys/"
+                         data-width="380"></div>
+                </div>
+                <div class="col-md-3 col-sm-12"><h3>Instagram</h3>
+                    To be added
+                </div>
 
-        <div class="col-md-3 col-sm-12"><h3>Twitter</h3>
-            To be added
-        </div>
-        <div class="col-md-3 col-sm-12"><h3>Subscribe to our news letter</h3>
-            To follow
-        </div>
+                <div class="col-md-3 col-sm-12"><h3>Twitter</h3>
+                    To be added
+                </div>
+                <div class="col-md-3 col-sm-12"><h3>Subscribe to our news letter</h3>
+                    To follow
+                </div>
 
+            </div>
+        </div>
     </div>
+    <div class="gray-section">
+        <div class="container text-white" style="">
+            <p class="copyright-text">Copyright © 2020 ToyNToys. All rights reserved</p>
+
+        </div>
+    </div>
+
 
 </section>
 
