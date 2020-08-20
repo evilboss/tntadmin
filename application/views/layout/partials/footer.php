90a6cd7ec463b15b2d1931ee9c7191691a893030
evilboss <jr.evilboss@gmail.com>
2020-08-21 00:09:10 +0800

    add complete social widgets

--- application/views/layout/partials/footer.php
+++ application/views/layout/partials/footer.php
@@ -1,34 +1,35 @@
 <section id="wrapper-footer" class="">
     <div class="dark-section">
         <div class="container text-white">
-            <div class="row  text-center header-wrapper">
+            <div class="row  header-wrapper">
                 <div class="col-lg-3 col-md-12 col-sm-12 ">
                     <h3 class="mb-4">Facebook</h3>
 
 
-
                     <div class="fb-page" data-href="https://www.facebook.com/toyntoys" data-tabs="timeline"
                          data-width="" data-height="410" data-small-header="false" data-adapt-container-width="true"
-                         data-hide-cover="false" data-show-facepile="true">
+                         data-hide-cover="false" data-show-facepile="true"
+                         data-pin-scale-width="80"
+                    >
                         <blockquote cite="https://www.facebook.com/toyntoys/" class="fb-xfbml-parse-ignore"><a
                                     href="https://www.facebook.com/toyntoys/">Toyntoys</a></blockquote>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-12 col-sm-12"><h3>Instagram</h3>
-                    <div class="fb-page"
-                         data-tabs="events"
-                         data-href="https://www.instagram.com/toyntoysasia/"
-                         data-width="380"></div>
+                <div class="col-lg-3 col-md-12 col-sm-12"><h3 class="mb-4">Pinterest</h3>
+                    <a data-pin-do="embedUser" data-adapt-container-width="true" data-height="410"
+                       data-pin-scale-width="80" href="https://www.pinterest.ph/danny1272/"></a>
                 </div>
 
-                <div class="col-lg-3 col-md-12 col-sm-12"><h3>Twitter</h3>
+                <div class="col-lg-3 col-md-12 col-sm-12"><h3 class="mb-4">Twitter</h3>
                     <a class="twitter-timeline" data-theme="dark"
+                       data-height="410"
                        href="https://twitter.com/Toyntoys1?ref_src=twsrc%5Etfw">Tweets by Toyntoys1</a>
                     <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 
                     <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
                 </div>
-                <div class="col-lg-3 col-md-12 col-sm-12 "><h6>Subscribe to our Newsletter</h6>
+                <div class="col-lg-3 col-md-12 col-sm-12 ">
+                    <h3 class="mb-4">Subscribe to our Newsletter</h3>
                     To follow
                 </div>
 
--- application/views/layout/partials/header.php
+++ application/views/layout/partials/header.php
@@ -45,6 +45,7 @@
 <body>
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
         integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
         crossorigin="anonymous"></script>
@@ -56,6 +57,7 @@
         crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
+<script async defer src="//assets.pinterest.com/js/pinit.js"></script>
 <script src="<?= base_url('assets/libs/owl.carousel/dist/owl.carousel.min.js') ?>"></script>
 <script src="<?= base_url('assets/libs/lightgallery/dist/js/lightgallery.min.js') ?>"></script>
 
